9a465375fd8a9ea5dbeb766f6a756f7cab9fdab8
Dave Arnold <darnold@hashicorp.com>
2018-06-26 13:27:26 -0700

    adding descriptions to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "resource_group" {
-  value = "${azurerm_resource_group.AzureEnv.id}"
+  value       = "${azurerm_resource_group.AzureEnv.id}"
+  description = "ID of Created Resource Group"
 }
 
 output "virtual_network_id" {
-  value = "${azurerm_virtual_network.AzureEnv.id}"
+  value       = "${azurerm_virtual_network.AzureEnv.id}"
+  description = "ID of Created Network"
 }
 
 output "security_group" {
-  value = "${azurerm_network_security_group.AzureEnv.id}"
+  value       = "${azurerm_network_security_group.AzureEnv.id}"
+  description = "ID of Created Security Group"
 }
 
 output "rg_name" {
-  value = "${var.env}"
+  value       = "${var.env}"
+  description = "Name of Created Resource Group"
 }
