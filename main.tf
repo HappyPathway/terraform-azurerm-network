f5c71e7fa216ce179d9f171f1b01d9cb3f53c86b
Dave Arnold <darnold@hashicorp.com>
2018-06-26 17:04:16 -0700

    changing name of security group

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "AzureEnv" {
 }
 
 resource "azurerm_network_security_group" "AzureEnv" {
-  name                = "acceptanceTestSecurityGroup1"
+  name                = "${var.env}"
   location            = "${azurerm_resource_group.AzureEnv.location}"
   resource_group_name = "${azurerm_resource_group.AzureEnv.name}"
 }
