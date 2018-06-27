3ba12566ca2f5c86b9f7aac6b185e44417c4d75b
Dave Arnold <darnold@hashicorp.com>
2018-06-26 17:25:50 -0700

    fixing

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "azurerm_virtual_network" "AzureEnv" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "${var.env}-subnet"
+  name                 = "${var.env}"
   resource_group_name  = "${azurerm_resource_group.AzureEnv.name}"
   virtual_network_name = "${azurerm_virtual_network.AzureEnv.name}"
   address_prefix       = "${var.subnet_cidr}"
