c18b9714fd7bd31d8835db5298a76436fd51cb12
Dave Arnold <darnold@hashicorp.com>
2018-06-26 17:17:17 -0700

    fixing stuff

--- variables.tf
+++ variables.tf
@@ -10,14 +10,18 @@ variable "location" {
   default     = "West US"
 }
 
-variable "address_space" {
+variable "network_cidr" {
   default     = "10.0.0.0/16"
   type        = "string"
   description = "Azure Network Address Space"
 }
 
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
 variable "dns_server" {
-  default     = "205.251.197.200" # example AWS DNS NS Server
+  default     = "205.251.197.200"      # example AWS DNS NS Server
   description = "External DNS Servers"
   type        = "string"
 }
