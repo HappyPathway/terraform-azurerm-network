dbfb7a8a6ffc277687d1a8a495bb96e02cb296ff
Dave Arnold <darnold@hashicorp.com>
2018-06-26 17:29:31 -0700

    changing back to automatic subnets

--- main.tf
+++ main.tf
@@ -16,16 +16,24 @@ resource "azurerm_network_security_group" "AzureEnv" {
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "AzureEnv" {
   name                = "${var.env}-network"
-  address_space       = ["${var.network_cidr}"]
+  address_space       = ["${var.address_space}"]
   location            = "${azurerm_resource_group.AzureEnv.location}"
   resource_group_name = "${azurerm_resource_group.AzureEnv.name}"
   dns_servers         = ["${var.dns_server}", "8.8.8.8"]
   tags                = "${var.resource_tags}"
-}
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "${var.env}"
-  resource_group_name  = "${azurerm_resource_group.AzureEnv.name}"
-  virtual_network_name = "${azurerm_virtual_network.AzureEnv.name}"
-  address_prefix       = "${var.subnet_cidr}"
+  subnet {
+    name           = "${var.env}-subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "${var.env}-subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  subnet {
+    name           = "${var.env}-subnet3"
+    address_prefix = "10.0.3.0/24"
+  }
 }
